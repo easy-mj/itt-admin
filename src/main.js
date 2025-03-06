2522e442a6a107ac2f258a056e3278ba15683148
mingjiu <boss_think@163.com>
2025-03-06 10:03:52 +0800

    feat: add 美化登录页面样式

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
 
+// 初始化样式表
+import '@/style/index.scss'
+
 const app = createApp(App)
 installElementPlus(app)
 app.use(store).use(router).mount('#app')
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,14 +9,22 @@
         <span class="svg-container">
           <el-icon><avatar /></el-icon>
         </span>
-        <el-input placeholder="username" name="username" type="text"></el-input>
+        <el-input
+          v-model="username"
+          placeholder="用户名"
+          name="username"
+        ></el-input>
       </el-form-item>
       <!-- 密码 -->
       <el-form-item>
         <span class="svg-container">
           <el-icon><avatar /></el-icon>
         </span>
-        <el-input placeholder="password" name="password" type="text"></el-input>
+        <el-input
+          v-model="password"
+          placeholder="密码"
+          name="password"
+        ></el-input>
         <span class="show-password">
           <el-icon><avatar /></el-icon>
         </span>
@@ -29,9 +37,90 @@
 
 <script setup>
 import { Avatar } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+const username = ref('')
+const password = ref('')
 </script>
 
 <style lang="scss" scoped>
+$bg: linear-gradient(to bottom, #0077be, #005588, #003366);
+$dark_gray: #889aa4;
+$light_gray: #eee;
+$cursor: #fff;
+
+.login-container {
+  width: 100%;
+  min-height: 100%;
+  background: $bg;
+  overflow: hidden;
+
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+
+    ::v-deep .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background-color: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+
+    ::v-deep .el-input {
+      height: 47px;
+      width: 85%;
+
+      .el-input__wrapper {
+        background: transparent;
+        box-shadow: none;
+      }
+
+      input {
+        background: transparent;
+        appearance: none;
+        -webkit-appearance: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        height: 47px;
+        caret-color: $cursor;
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+
+  .show-password {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+}
+
 .login-btn {
   width: 100%;
   margin-bottom: 30px;
