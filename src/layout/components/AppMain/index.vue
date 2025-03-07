f760bdc83a5026d6184c394955df97970041f31b
mingjiu <boss_think@163.com>
2025-03-07 11:54:57 +0800

    feat: add 创建基于Layout的基础架构

---
+++ src/layout/components/AppMain/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app-main">AppMain</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.app-main {
+  position: relative;
+  width: 100%;
+  // 浏览器可视区域高度100vh
+  height: calc(100vh - 50px);
+  padding: 61px 20px 20px 20px;
+  box-sizing: border-box;
+  overflow: hidden;
+}
+</style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,7 +1,46 @@
 <template>
-  <div>Layout 页面</div>
+  <div class="app-wrapper">
+    <!-- 左侧 Menu -->
+    <side-bar
+      id="guide-sidebar"
+      class="sidebar-container"
+      :style="{ backgroundColor: variables.menuBg }"
+    ></side-bar>
+    <div class="main-container">
+      <div class="fixed-header">
+        <!-- 顶部的 Navbar -->
+        <navbar></navbar>
+      </div>
+      <!-- 主内容区 -->
+      <app-main></app-main>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import variables from '@/style/variables.module.scss'
+import Navbar from './components/Navbar'
+import SideBar from './components/Sidebar'
+import AppMain from './components/AppMain'
+console.log(variables)
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '~@/style/mixin.scss';
+@import '~@/style/variables.module.scss';
+
+.app-wrapper {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  @include clearfix;
+}
+
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+}
+</style>
