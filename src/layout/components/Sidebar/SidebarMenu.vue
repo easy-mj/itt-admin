ab49989ceb2f08b024c1dc544c7d86c726919656
mingjiu <boss_think@163.com>
2025-03-07 18:20:24 +0800

    feat: add 创建临时 menu 菜单

---
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-menu
+    :unique-opened="true"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+  >
+    <!-- 子级 Menu -->
+    <el-sub-menu index="1">
+      <template #title>
+        <el-icon><Location /></el-icon>
+        <span>导航一</span>
+      </template>
+      <el-menu-item index="1-1">选项1</el-menu-item>
+      <el-menu-item index="1-2">选项2</el-menu-item>
+    </el-sub-menu>
+    <!-- 具体菜单 -->
+    <el-menu-item index="2">
+      <template #title>
+        <el-icon><Location /></el-icon>
+        <span>导航二</span>
+      </template>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>SideBar</div>
+  <div>
+    <h3>后台系统</h3>
+    <el-scrollbar>
+      <sidebar-menu></sidebar-menu>
+    </el-scrollbar>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import SidebarMenu from './SidebarMenu.vue'
+</script>
 
 <style lang="scss" scoped></style>
