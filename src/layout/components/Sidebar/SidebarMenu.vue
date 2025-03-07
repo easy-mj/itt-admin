85331c9d6d3436cd1f325818f064135c5fdbfabb
mingjiu <boss_think@163.com>
2025-03-07 22:02:33 +0800

    feat: add 创建结构路由表

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -1,26 +1,62 @@
 <template>
   <el-menu
+    :router="true"
     :unique-opened="true"
     background-color="#545c64"
     text-color="#fff"
     active-text-color="#ffd04b"
   >
-    <!-- 子级 Menu -->
-    <el-sub-menu index="1">
+    <!-- 个人中心 -->
+    <el-menu-item index="/profile">
       <template #title>
-        <el-icon><Location /></el-icon>
-        <span>导航一</span>
-      </template>
-      <el-menu-item index="1-1">选项1</el-menu-item>
-      <el-menu-item index="1-2">选项2</el-menu-item>
-    </el-sub-menu>
-    <!-- 具体菜单 -->
-    <el-menu-item index="2">
-      <template #title>
-        <el-icon><Location /></el-icon>
-        <span>导航二</span>
+        <svg-icon icon="user"></svg-icon>
+        <span>个人中心</span>
       </template>
     </el-menu-item>
+    <!-- 用户 -->
+    <el-sub-menu index="/user">
+      <template #title>
+        <svg-icon icon="personnel"></svg-icon>
+        <span>用户</span>
+      </template>
+      <el-menu-item index="/user/manage">
+        <template #title>
+          <svg-icon icon="personnel-manage"></svg-icon>
+          <span>员工管理</span>
+        </template>
+      </el-menu-item>
+      <el-menu-item index="/user/role">
+        <template #title>
+          <svg-icon icon="role"></svg-icon>
+          <span>角色列表</span>
+        </template>
+      </el-menu-item>
+      <el-menu-item index="/user/permission">
+        <template #title>
+          <svg-icon icon="permission"></svg-icon>
+          <span>权限列表</span>
+        </template>
+      </el-menu-item>
+    </el-sub-menu>
+    <!-- 文章 -->
+    <el-sub-menu index="/article">
+      <template #title>
+        <svg-icon icon="article"></svg-icon>
+        <span>文章</span>
+      </template>
+      <el-menu-item index="/article/ranking">
+        <template #title>
+          <svg-icon icon="article-ranking"></svg-icon>
+          <span>文章排名</span>
+        </template>
+      </el-menu-item>
+      <el-menu-item index="/article/create">
+        <template #title>
+          <svg-icon icon="article-create"></svg-icon>
+          <span>创建文章</span>
+        </template>
+      </el-menu-item>
+    </el-sub-menu>
   </el-menu>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,81 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import layout from '@/layout'
+
+/**
+ * 私有路由表
+ */
+const privateRoutes = [
+  {
+    path: '/user',
+    redirect: '/user/manage',
+    meta: { title: 'user', icon: 'personnel' },
+    component: layout,
+    children: [
+      {
+        name: 'userManage',
+        path: '/user/manage',
+        meta: { title: 'userManage', icon: 'personnel-manage' },
+        component: () => import('@/views/user-manage')
+      },
+      {
+        name: 'userRole',
+        path: '/user/role',
+        meta: { title: 'roleList', icon: 'role' },
+        component: () => import('@/views/role-list')
+      },
+      {
+        name: 'userPermission',
+        path: '/user/permission',
+        meta: { title: 'permissionList', icon: 'permission' },
+        component: () => import('@/views/permission-list')
+      },
+      {
+        name: 'userInfo',
+        path: '/user/info/:id',
+        meta: { title: 'userInfo' },
+        component: () => import('@/views/user-info')
+      },
+      {
+        name: 'import',
+        path: '/user/import',
+        meta: { title: 'excelImport' },
+        component: () => import('@/views/import')
+      }
+    ]
+  },
+  {
+    path: '/article',
+    redirect: '/article/ranking',
+    component: layout,
+    meta: { title: 'article', icon: 'article' },
+    children: [
+      {
+        name: 'articleRanking',
+        path: '/article/ranking',
+        meta: { title: 'articleRanking', icon: 'article-ranking' },
+        component: () => import('@/views/article-ranking')
+      },
+      {
+        name: 'articleDetail',
+        path: '/article/:id',
+        meta: { title: 'articleDetail' },
+        component: () => import('@/views/article-detail')
+      },
+      {
+        name: 'articleCreate',
+        path: '/article/create',
+        meta: { title: 'articleCreate', icon: 'article-create' },
+        component: () => import('@/views/article-create')
+      },
+      {
+        name: 'articleEdit',
+        path: '/article/edit/:id',
+        meta: { title: 'articleEdit' },
+        component: () => import('@/views/article-create')
+      }
+    ]
+  }
+]
 
 /**
  * 公开路由表
@@ -10,13 +87,35 @@ const publicRoutes = [
   },
   {
     path: '/',
-    component: () => import('@/layout')
+    redirect: '/profile',
+    component: layout,
+    children: [
+      // 个人中心
+      {
+        path: '/profile',
+        name: 'profile',
+        meta: { title: 'profile', icon: 'personnel' },
+        component: () => import('@/views/profile')
+      },
+      // 404
+      {
+        path: '/404',
+        name: '404',
+        component: () => import('@/views/error-page/404')
+      },
+      // 401
+      {
+        path: '/401',
+        name: '401',
+        component: () => import('@/views/error-page/401')
+      }
+    ]
   }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: publicRoutes
+  routes: [...publicRoutes, ...privateRoutes]
 })
 
 export default router
