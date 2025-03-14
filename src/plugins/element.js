084f4a2605dea950a3e7543a2da7ee82527cea4f
mingjiu <boss_think@163.com>
2025-03-14 09:26:06 +0800

    feat: add element-plus 国际化处理

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,10 +1,13 @@
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import en from 'element-plus/es/locale/lang/en'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import store from '@/store'
 
 export default (app) => {
-  app.use(ElementPlus, { locale: zhCn })
+  // 根据当前语言选择使用哪种语言包
+  app.use(ElementPlus, { locale: store.getters.language === 'zh' ? zhCn : en })
 
   // 注册 Element 所有图标
   for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="profile">个人中心{{ $t('message.hello') }}</div>
+  <el-pagination
+    v-model:current-page="currentPage"
+    :page-size="100"
+    :size="size"
+    :disabled="disabled"
+    :background="background"
+    layout="total, prev, pager, next"
+    :total="1000"
+    @size-change="handleSizeChange"
+    @current-change="handleCurrentChange"
+  />
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const currentPage = ref(5)
+const background = ref(false)
+const disabled = ref(false)
+
+const handleSizeChange = (val) => {
+  console.log(`${val} items per page`)
+}
+const handleCurrentChange = (val) => {
+  console.log(`current page: ${val}`)
+}
+</script>
 
 <style lang="scss" scoped></style>
