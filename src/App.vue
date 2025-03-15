3bcf97780cfe87fd1a3b6aae24c4554534e90052
mingjiu <boss_think@163.com>
2025-03-15 10:27:10 +0800

    feat: add element-plus 新主题的立即生效

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,15 @@
   <router-view></router-view>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
+<script setup>
+import { useStore } from 'vuex'
+import { generateNewStyle, writeNewStyle } from '@/utils/theme'
+
+// 根据当前主题色写入新 style 样式
+const store = useStore()
+generateNewStyle(store.getters.themeColor).then((newStyle) => {
+  writeNewStyle(newStyle)
+})
 </script>
 
 <style></style>
