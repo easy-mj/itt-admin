bc9a2677aac35f9f4de54ef4c931ae823c687a12
mingjiu <boss_think@163.com>
2025-03-15 17:14:45 +0800

    feat: add 章节模块

--- src/views/profile/components/Chapter.vue
+++ src/views/profile/components/Chapter.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>章节</div>
+  <el-timeline>
+    <el-timeline-item
+      v-for="item in chapterData"
+      :key="item.id"
+      :timestamp="item.timestamp"
+      placement="top"
+    >
+      <el-card>
+        <h4>{{ item.content }}</h4>
+      </el-card>
+    </el-timeline-item>
+  </el-timeline>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { getChapter } from '@/api/user'
+import { watchSwitchLanguage } from '@/utils/i18n'
+
+// 获取章节数据
+const chapterData = ref(null)
+const fetchChapter = async () => {
+  const res = await getChapter()
+  chapterData.value = res
+}
+fetchChapter()
+
+// 切换语言后重新获取数据
+watchSwitchLanguage(() => {
+  fetchChapter()
+})
+</script>
 
 <style lang="scss" scoped></style>
