b88f3152dee529e58bc684ed07367fa0e638acf5
mingjiu <boss_think@163.com>
2025-03-16 19:16:07 +0800

    feat: add 实现 Excel 导出逻辑

--- src/components/Export2Excel/index.vue
+++ src/components/Export2Excel/index.vue
@@ -29,6 +29,8 @@ import { ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { watchSwitchLanguage } from '@/utils/i18n'
 import { getUserManageAllList } from '@/api/user-manage'
+import { dateFilter } from '@/filter'
+import { USER_RELATIONS } from './helper'
 
 defineProps({
   modelValue: {
@@ -60,11 +62,48 @@ const handleClose = () => {
 
 // 点击确定
 const exportLoading = ref(false)
+
+/**
+ * 使用 export_json_excel 的时候，需要的数据格式必须是二维数组
+ * 格式：[['', '', '', ''], ['', '', '', ''], ['', '', '', '']]
+ * 其中 ['', '', '', ''] 代表每行数据，里面的每个元素代表当前行每列的数据
+ */
+const formatJSON = (headers, rows = []) => {
+  return rows.map((item) => {
+    return Object.keys(headers).map((key) => {
+      // 角色字段需要特殊处理（角色是个数组）
+      if (headers[key] === 'role') {
+        const roles = item[headers[key]]
+        return JSON.stringify(roles.map((r) => r.title))
+      }
+      // 时间字段需要特殊处理
+      if (headers[key] === 'openTime') {
+        return dateFilter(item[headers[key]])
+      }
+      return item[headers[key]]
+    })
+  })
+}
+
 const handleConfirm = async () => {
   exportLoading.value = true
   try {
-    const allUsers = await getUserManageAllList()
-    console.log(allUsers)
+    // 获取数据
+    const result = await getUserManageAllList()
+    const allUsers = result.list
+    // 导入工具包(异步导入)
+    const excelUtils = await import('@/utils/Export2Excel')
+    // 格式化数据
+    const data = formatJSON(USER_RELATIONS, allUsers)
+    // 导出
+    excelUtils.export_json_to_excel({
+      // 表头
+      header: Object.keys(USER_RELATIONS),
+      // 数据
+      data,
+      // 文件名称
+      filename: excelName.value || exportDefaultName
+    })
     closeDialog()
   } catch (error) {
   } finally {
