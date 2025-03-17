ca36f9a63acc099c1a9d98c0c3de60b70eef0093
mingjiu <boss_think@163.com>
2025-03-17 20:00:12 +0800

    feat: add 角色分配权限

--- src/api/role.js
+++ src/api/role.js
@@ -8,3 +8,23 @@ export const getRoleList = () => {
     url: '/role/list'
   })
 }
+
+/**
+ * 获取当前角色对应的权限
+ */
+export const getRolePermissions = (roleId) => {
+  return request({
+    url: `/role/permission/${roleId}`
+  })
+}
+
+/**
+ * 为角色分配权限
+ */
+export const distributePermission = (data) => {
+  return request({
+    url: '/role/distribute-permission',
+    method: 'POST',
+    data
+  })
+}
--- src/views/role-list/index.vue
+++ src/views/role-list/index.vue
@@ -12,19 +12,29 @@
         <el-table-column :label="$t('msg.role.name')" prop="title"></el-table-column>
         <el-table-column :label="$t('msg.role.desc')" prop="describe"></el-table-column>
         <el-table-column :label="$t('msg.role.action')" width="200">
-          <el-button type="primary" size="small">
-            {{ $t('msg.role.assignPermissions') }}
-          </el-button>
+          <template #default="{ row }">
+            <el-button type="primary" size="small" @click="handleAssignPermissionClick(row)">
+              {{ $t('msg.role.assignPermissions') }}
+            </el-button>
+          </template>
         </el-table-column>
       </el-table>
     </el-card>
+    <!-- 分配权限弹窗 -->
+    <assign-permission-dialog
+      v-model="assignPermissionVisible"
+      :roleId="selectRoleId"
+      @updatePermissionSuccess="fetchRoleList"
+    >
+    </assign-permission-dialog>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { getRoleList } from '@/api/role'
 import { watchSwitchLanguage } from '@/utils/i18n'
+import AssignPermissionDialog from './components/AssignPermission'
 
 // 角色列表
 const allRoles = ref([])
@@ -41,6 +51,19 @@ const fetchRoleList = async () => {
 }
 fetchRoleList()
 watchSwitchLanguage(fetchRoleList) // 国际化
+
+// 分配权限弹窗
+const assignPermissionVisible = ref(false)
+const selectRoleId = ref('')
+const handleAssignPermissionClick = (row) => {
+  assignPermissionVisible.value = true
+  selectRoleId.value = row.id
+}
+
+// 关闭弹窗清除选择的roleId
+watch(assignPermissionVisible, (val) => {
+  if (!val) selectRoleId.value = ''
+})
 </script>
 
 <style lang="scss" scoped></style>
